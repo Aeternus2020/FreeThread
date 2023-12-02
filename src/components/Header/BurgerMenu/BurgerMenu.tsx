f2155ede98d56585d8ea55a706a0f01b501609d2
Alena <crimeaodessa2006Qgmail.com>
2023-12-02 17:02:51 +0100

    add translate for about, header, subheader

--- src/components/Header/BurgerMenu/BurgerMenu.tsx
+++ src/components/Header/BurgerMenu/BurgerMenu.tsx
@@ -1,4 +1,5 @@
 import React, { FC } from 'react';
+import { useTranslation } from 'react-i18next';
 import { IconButton, MenuItem, Collapse } from '@mui/material';
 import {
 	CustomizedMenu,
@@ -35,6 +36,7 @@ const BurgerMenu: FC<HeaderBurgerMenuProps> = React.memo(
 		isActive,
 	}) => {
 		const openBurgerMenu = Boolean(burgerMenu);
+		const { t } = useTranslation();
 
 		return (
 			<>
@@ -64,78 +66,50 @@ const BurgerMenu: FC<HeaderBurgerMenuProps> = React.memo(
 					<CatalogLink onClick={toggleActive}>
 						<MenuItem disableRipple divider onClick={handleOpenSubMenu}>
 							<BtnCatalogLine $active={isActive} />
-							Каталог
+							{t('header.catalog')}
 							{!openSubMenu ? <ArrowRight /> : <ArrowDown />}
 						</MenuItem>
 					</CatalogLink>
 					<Collapse in={openSubMenu} timeout="auto" unmountOnExit>
 						<SubMenuItem to="/kiev">
-							<MenuItem
-								disableRipple
-								divider
-								onClick={() => {
-									handleCloseBurgerMenu();
-									handleOpenSubMenu();
-								}}
-							>
-								“Сорочки Київщини”
+							<MenuItem disableRipple divider onClick={handleCloseBurgerMenu}>
+								{t('collection.kiev.title')}
 							</MenuItem>
 						</SubMenuItem>
 						<SubMenuItem to="regions">
-							<MenuItem
-								disableRipple
-								divider
-								onClick={() => {
-									handleCloseBurgerMenu();
-									handleOpenSubMenu();
-								}}
-							>
-								“Регіони України”
+							<MenuItem disableRipple divider onClick={handleCloseBurgerMenu}>
+								{t('collection.regions.title')}
 							</MenuItem>
 						</SubMenuItem>
 						<SubMenuItem to="/grandmother">
-							<MenuItem
-								disableRipple
-								divider
-								onClick={() => {
-									handleCloseBurgerMenu();
-									handleOpenSubMenu();
-								}}
-							>
-								“Віднови вишиванку своєї бабусі”
+							<MenuItem disableRipple divider onClick={handleCloseBurgerMenu}>
+								{t('collection.grandmother.title')}
 							</MenuItem>
 						</SubMenuItem>
 						<SubMenuItem to="/accessories">
-							<MenuItem
-								disableRipple
-								divider
-								onClick={() => {
-									handleCloseBurgerMenu();
-									handleOpenSubMenu();
-								}}
-							>
-								Аксесуари
+							<MenuItem disableRipple divider onClick={handleCloseBurgerMenu}>
+								{t('collection.accessories.title')}
 							</MenuItem>
 						</SubMenuItem>
 					</Collapse>
 					<CustomLink to="/about">
 						<MenuItem disableRipple divider onClick={handleCloseBurgerMenu}>
-							Про нас
+							{t('about.title')}
 						</MenuItem>
 					</CustomLink>
 					<CustomLink to="/delivery">
 						<MenuItem disableRipple divider onClick={handleCloseBurgerMenu}>
-							Доставка і оплата
+							{t('delivery.title')}
 						</MenuItem>
 					</CustomLink>
 					<CustomLink to="/comments">
 						<MenuItem divider onClick={handleCloseBurgerMenu}>
-							Відгуки
+							{t('header.button.reviews')}
 						</MenuItem>
 					</CustomLink>
 					<CustomLink to="/contacts">
 						<MenuItem disableRipple divider onClick={handleCloseBurgerMenu}>
-							Контакти
+							{t('header.button.contacts')}
 						</MenuItem>
 					</CustomLink>
 				</CustomizedMenu>
